9fe97901284f2c356f1558dfcb3c060b0bdf1c09
Derik Hammer <derik@sqlhammer.com>
2017-08-19 09:30:18 -0400

    Initial commit after getting the most basic CombatScript working.

---
+++ Singularity/TestGUI/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TestGUI
+{
+    partial class frmTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtScript = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtScript
+            // 
+            this.txtScript.Location = new System.Drawing.Point(52, 42);
+            this.txtScript.Multiline = true;
+            this.txtScript.Name = "txtScript";
+            this.txtScript.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtScript.Size = new System.Drawing.Size(893, 354);
+            this.txtScript.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(370, 420);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(238, 115);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(52, 571);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(893, 425);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // frmTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 1060);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtScript);
+            this.Name = "frmTester";
+            this.Text = "Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtScript;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Singularity/TestGUI/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using CombatScript;
+
+namespace TestGUI
+{
+    public partial class frmTester : Form
+    {
+        public frmTester()
+        {
+            InitializeComponent();
+        }
+
+        public void Run()
+        {
+            string script = txtScript.Text;
+            string errorMsg = null;
+
+            try
+            {
+                Interpreter.Instance.Process(script);
+            }
+            catch (ArgumentException exc)
+            {
+                errorMsg = exc.Message;
+            }
+            
+            string output = Interpreter.Instance.Output;
+            if (!string.IsNullOrWhiteSpace(output))
+            {
+                txtOutput.Text = output;
+            }
+            if (!string.IsNullOrWhiteSpace(errorMsg))
+            {
+                txtOutput.Text = txtOutput.Text + Environment.NewLine + errorMsg;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+    }
+}
